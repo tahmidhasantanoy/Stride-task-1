7ceb5531b2e6c6c2f1c5bdf1429b7de06a8f40b0
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2024-05-26 15:57:23 +0600

    Navbar resize

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Components/Layouts/MainLayout";
 import Home from "../Pages/Home/Home";
+import AddProducts from "../Pages/AddProducts/AddProducts";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
         index: true,
         element: <Home />,
       },
+      {
+        path : "/add-products",
+        element : <AddProducts/>
+      }
     ],
   },
 ]);
--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,9 +1,23 @@
-import Container from "../../Components/Ui/Container";
-import Logo from "./Logo";
+// import Container from "../../Components/Ui/Container";
+// import Logo from "./Logo";
+import { Link } from "react-router-dom";
 import Profile from "./Profile";
-import Search from "./Search";
+// import Search from "./Search";
 
 const Header = () => {
+  const menu = (
+    <>
+      <li>
+        <Link>About</Link>
+      </li>
+      <li>
+        <Link to={"/add-products"}>Add Products</Link>
+      </li>
+      <li>
+        <Link>Dashboard</Link>
+      </li>
+    </>
+  );
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -26,51 +40,15 @@ const Header = () => {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 "
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {menu}
           </ul>
         </div>
         <a className="btn btn-ghost text-xl">GollaChut</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+        <ul className="menu menu-horizontal px-1">{menu}</ul>
       </div>
       <div className="navbar-end">
         <Profile />
